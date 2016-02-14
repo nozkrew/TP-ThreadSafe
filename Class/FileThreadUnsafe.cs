c56d8c17c70839c62efeeef3bc036a6bf40110e3
Nozkrew <thomas.maire1@hotmail.fr>
2016-02-14 13:01:49 +0100

    First commit

---
+++ Class/FileThreadUnsafe.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class FileThreadUnsafe<T>
+    {
+        private T[] tab;
+        private int tete, queue;
+        static readonly object verrou = new object();
+        public FileThreadUnsafe(int taille)
+        {
+            tab = new T[taille];
+            Init();
+        }
+        private int Suivant(int i)
+        {
+            lock (verrou)
+                return (i + 1) % tab.Length;
+        }
+        private void Init()
+        {
+            lock (verrou)
+                tete = queue = -1;
+        }
+        public void Enfiler(T element)
+        {
+            if (Pleine())
+                throw new ExceptionFilePleine();
+            else if (Vide())
+                tab[queue = tete = 0] = element;
+            else
+                tab[queue = Suivant(queue)] = element;
+        }
+        public void Defiler()
+        {
+            lock (verrou)
+            {
+                if (Vide())
+                    throw new ExceptionFileVide();
+                else if (NbElements() == 1)
+                    Init();
+                else
+                    tete = Suivant(tete);
+            }
+        }
+
+        public bool Vide()
+        {
+            return (tete == -1) && (queue == -1);
+        }
+        public bool Pleine()
+        {
+            return Suivant(queue) == tete;
+        }
+
+        public int NbElements()
+        {
+            lock (verrou)
+            {
+                if (Vide())
+                    return 0;
+                else if (tete <= queue)
+                    return queue - tete + 1;
+                else
+                    return tab.Length + queue - tete + 1;
+            }
+        }
+
+        public T Premier() {
+            lock (verrou)
+            {
+                if (Vide())
+                    throw new ExceptionFileVide();
+                else
+                    return tab[tete];
+            }
+        }
+    }
+    public class ExceptionFileVide : Exception { }
+    public class ExceptionFilePleine : Exception { }
+}
---
+++ TP4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Class;
+using System.Threading;
+
+namespace TP4
+{
+    class Program
+    {
+        static Thread th1;
+        static Thread th2;
+        static FileThreadUnsafe<int> f;
+        static void Main(string[] args)
+        {
+            f = new FileThreadUnsafe<int>(10);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                try
+                {
+                    f.Enfiler(i);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erreur : " + e.Message);
+                }
+            }
+            AfficherNBElement(f.NbElements());
+
+            th1 = new Thread(Lancement1);
+            th2 = new Thread(Lancement2);
+
+            th1.Start();
+            th2.Start();
+
+            th1.Join();
+            th2.Join();
+
+            Console.ReadLine();
+        }
+
+        public static void Lancement1()
+        {
+            Console.WriteLine("Lancement 1");
+            if (!f.Vide())
+            {
+                try
+                {
+                    f.Defiler();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Erreur " + e.Message);
+                }
+            }
+            AfficherNBElement(f.NbElements());
+        }
+
+        public static void Lancement2()
+        {
+            Console.WriteLine("Lancement 2");
+            while (!f.Vide())
+            {
+                try
+                {
+                    f.Defiler();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Erreur " + e.Message);
+                }
+
+            }
+            AfficherNBElement(f.NbElements());
+        }
+
+        public static void AfficherNBElement(int nb)
+        {
+            Console.WriteLine("Nb elements : " + nb);
+        }
+    }
+}
